058c598dfddc8bcd028050fafcccc4284297257e
arshi091206 <gargarshi09@gmail.com>
2026-06-30 13:38:17 +0530

    create public landing page and dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -36,7 +36,7 @@ def login_view(request):
 
         if user is not None:
             login(request,user)
-            return redirect("/")
+            return redirect("/dashboard/")
         
         messages.error(
             request,
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from alerts.models import Alert
 
 # Create your views here.
+def landing(request):
+    return render(
+        request,
+        "dashboard/landing.html"
+    )
 
 def home(request):
     print(request.user)
+
+    alerts=Alert.objecets.all()
     return render(
         request,
-        "dashboard/home.html"
+        "dashboard/home.html",
+        {
+            "alerts":alerts
+        }
     )
