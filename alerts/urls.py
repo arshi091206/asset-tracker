0cb40bbef101633590a1d8c040d09c539b7d4cda
arshi091206 <gargarshi09@gmail.com>
2026-07-03 16:27:46 +0530

    implement alert edit and delete functionality

--- alerts/urls.py
+++ alerts/urls.py
@@ -6,4 +6,12 @@ urlpatterns=[
         "create/",
         views.create_alert
     ),
+    path(
+        "delete/<int:alert_id>/",
+        views.delete_alert
+    ),
+    path(
+        "edit/<int:alert_id>/",
+        views.edit_alert
+    )
 ]
--- alerts/views.py
+++ alerts/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from .models import Alert
 from django.contrib.auth.decorators import login_required
 
@@ -36,4 +36,51 @@ def create_alert(request):
         return redirect("/dashboard/")
     
 
+@login_required
+def delete_alert(request, alert_id):
+    alert=get_object_or_404(
+        Alert,
+        id=alert_id,
+        user=request.user
+    )
+
+    alert.delete()
+
+    messages.success(
+        request,
+        "Alert deleted successfully"
+    )
+
+    return redirect("/dashboard/")
+
+@login_required
+def edit_alert(request, alert_id):
+    alert=get_object_or_404(
+        Alert,
+        id=alert_id,
+        user=request.user
+    )
+
+    if request.method=="GET":
+        return render(
+            request,
+            "alerts/edit_alert.html",
+            {
+                "alert":alert
+            }
+        )
     
+    if request.method=="POST":
+        alert.ticker=request.POST["ticker"]
+        alert.asset_type=request.POST["asset_type"]
+        alert.condition=request.POST["condition"]
+        alert.target_price=request.POST["target_price"]
+
+        alert.save()
+
+        messages.success(
+            request,
+            "Alert updated successfully!"
+        )
+
+        return redirect("/dashboard/")
