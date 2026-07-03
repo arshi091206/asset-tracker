263435d3e9829452cab5093e122f988892363fa5
arshi091206 <gargarshi09@gmail.com>
2026-07-03 16:07:24 +0530

    Added alert confirmation, updated UI

--- alerts/views.py
+++ alerts/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from .models import Alert
 from django.contrib.auth.decorators import login_required
@@ -16,12 +17,21 @@ def create_alert(request):
         condition=request.POST["condition"]
         target_price=request.POST["target_price"]
 
+        print("Ticker:", ticker)
+        print("Asset Type:", asset_type)
+        print("Condition:", condition)
+        print("Target Price:", target_price)
         Alert.objects.create(
             user=request.user,
             ticker=ticker,
             asset_type=asset_type,
             condition=condition,
             target_price=target_price
+        )
+
+        messages.success(
+            request,
+            "Alert created successfully!"
         )
         return redirect("/dashboard/")
     
--- asset_tracker/settings.py
+++ asset_tracker/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'asset_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
